e472c62924970b390e9524e299bacc5da24a9e19
Sushmashreeps10 <sushre17@gmail.com>
2025-03-05 17:27:10 +0530

    Create BMI.js

---
+++ BMI.js
@@ -0,0 +1,11 @@
+function bmi(w,h){
+    var count = h*h;
+    // var count2 = m*m;
+    var res = w/count;
+    var round = Math.floor(res);
+    // console.log(round);
+    console.log(res);
+    
+}
+
+bmi(83,1.9)
