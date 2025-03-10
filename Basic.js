3d6f52416edd474ced9373c27300efcebdb3ea13
Sushmashreeps10 <sushre17@gmail.com>
2025-03-10 11:49:12 +0530

    Basic.js

--- Basic.js
+++ Basic.js
@@ -1,9 +1,27 @@
 Basic of js
 
-variables:-----------
+// variables
+
 full_name = "sushma";
 age = 21;
 a = null;
 b = undefined;
 place = "bangalore";
 console.log(full_name);
+
+// arithmetic operators
+
+a = 10;
+b = 28;
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+
+// Objects
+
+const var = {
+  fullName : "Sushma",
+  Place : "Bangalore",
+  Number : 1234567890
+  };
