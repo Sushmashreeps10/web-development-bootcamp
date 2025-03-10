4e518fcbe230e4024129b6188e02b4a98d95ccf2
Sushmashreeps10 <sushre17@gmail.com>
2025-03-10 13:10:04 +0530

    Basic.js

--- Basic.js
+++ Basic.js
@@ -44,3 +44,29 @@ if (a <= 100 && a >=80){
 }
 
 console.log("The grade of the student is:- ", grade)
+
+// loops-forloop
+let sum = 0;
+for (let i =0; i<=10; i++){
+  sum += 1;
+  console.log(i);
+}
+console.log("value of sum:-", sum);
+console.log("Lopp is ended");
+
+// while-do while loops
+let v = 2;
+do {
+    console.log("start the loops");
+}
+while ( v <=5){
+    // v += 1;
+    console.log("condition  is true");
+}
+
+// for loops for strings
+var str = "sushma";
+
+for (var i of str){
+    console.log("i=",i);
+}
