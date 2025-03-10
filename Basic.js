9caf8f3520b0834034aad5379814221986b5d851
Sushmashreeps10 <sushre17@gmail.com>
2025-03-10 12:50:33 +0530

    Basic.js

--- Basic.js
+++ Basic.js
@@ -25,3 +25,22 @@ const var = {
   Place : "Bangalore",
   Number : 1234567890
   };
+
+// if/else
+
+let a = prompt("enter a num(0-100):- ");
+let grade;
+
+if (a <= 100 && a >=80){
+    grade = "A";
+} else if (a <= 89 && a >=70){
+    grade = "B";
+}else if (a <= 69 && a >=60){
+    grade = "C";
+}else if (a <= 59 && a >=50){
+    grade = "D";
+} else {
+    grade = "F";
+}
+
+console.log("The grade of the student is:- ", grade)
